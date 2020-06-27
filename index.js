7ffa64e943774f0aac0b80a9161f5a6aa4efaee1
Shubham <shubhamnagpal30@gmail.com>
2020-06-27 17:39:45 +0530

    Added flash messages using Noty.js

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
 
 app.use(sassMiddleware({
   src: './assets/scss',
@@ -58,6 +60,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
+
+app.use(flash());
+app.use(customMware.setFlash);
 
 // Use Express router
 app.use('/', require('./routes'));
