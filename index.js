ba027862225ca057076b77ff4833fc3a7bb2e4ae
Shubham <shubhamnagpal30@gmail.com>
2020-06-27 14:49:16 +0530

    Setup current authenticated user

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(passport.setAuthenticatedUser);
+
 // Use Express router
 app.use('/', require('./routes'));
 
