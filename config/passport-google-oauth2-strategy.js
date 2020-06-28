2cc5ee641891aaf3410116a31608cb1df6bb1375
Shubham <shubhamnagpal30@gmail.com>
2020-06-28 11:11:30 +0530

    Add social Authentication

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,46 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const crypto = require('crypto');
+const User = require('../models/user');
+
+// Tell passport to use a new strategy for google login
+passport.use(new googleStrategy({
+    clientID: "383911619781-0htn9hssfhf9q8ovo1e8a7u2hdlr373s.apps.googleusercontent.com",
+    clientSecret: "FOB4KkovzyvzORS-p3fQ7WQV",
+    callbackURL: "http://localhost:8000/users/auth/google/callback"
+  },
+
+  function(accessToken, refreshToken, profile, done){
+    // Find a user
+    User.findOne({email: profile.emails[0].value}).exec(function(err, user){
+      if(err){
+        console.log('error in google strategy-passport', err); 
+        return;
+      }
+      
+      console.log(accessToken, refreshToken);
+      console.log(profile);
+      if(user){
+        //  if found set this user as req.user
+        return done(null, user);
+      } else {
+        // If not found, create the user and set it as req.user
+        User.create({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          password: crypto.randomBytes(20).toString('hex')
+        }, function(err, user){
+          if(err){
+            console.log('error in creating user google strategy-passport', err); 
+            return;
+          }
+
+          return done(null, user);
+        });
+      }
+    });
+  }
+));
+
+
+module.exports = passport;
